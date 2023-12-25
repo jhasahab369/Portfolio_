5f50a1fdd23e4533cab8ea79fca36f80b7c17a4b
jhasahab <jhabk369@gmail.com>
2023-12-25 22:27:50 +0530

    finished

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+import logo from "./logo_fr.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import iit from "./company/iit.png";
+import robocon from "./company/robocon.png";
+import mathongo from "./company/mathongo.png";
+import icse from "./company/icse.png";
+import cbse from "./company/cbse.png";
+import freelance from "./company/freelance.png";
+import chegg from "./company/chegg.png";
+
+
+export {
+  iit, icse, cbse, robocon, mathongo, freelance, chegg,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/components/constants/index.js
@@ -0,0 +1,292 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    mathongo,
+    chegg,
+    freelance,
+    robocon,
+    icse,
+    cbse,
+    iit,
+  } from "/home/jhasaab/Desktop/3d_portfolio/src/assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Competetive Programmer",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    
+    {
+      title: "Data Analyst",
+      company_name: "MathonGo",
+      icon: mathongo,
+      iconBg: "#383E56",
+      date: "Feb 2023- March 2023",
+      points: [
+        "Analysed datas of competetive exam question types, and used classification mechanism to render them","Worked on ateam with 12 students in India, collaboratively to ensure the smooth workflow of content development","Was responsible for the crucial roles of multiple projects of creation of Question banks catering to different needs"
+      ],
+    },
+
+
+
+    
+    
+    
+    {
+      title: "CS- Subject Matter Expert",
+      company_name: "Chegg",
+      icon: chegg,
+      iconBg: "#383E56",
+      date: "January 2023- onwards",
+      points: [
+        "Resolving complex sophisticated programming related doubts","A student community which is culmination of intrguing questions","Employed Deep theoritical Computer Science knowledge posing boht the core and non core fundamentals"
+      ],
+    },
+    {
+      title: "Full Stack Developer",
+      company_name: "Freelance clients",
+      icon: freelance,
+      iconBg: "#E6DEDD",
+      date: " Sept 2023-onwards",
+      points: [
+        "Developing and maintaining web applications using React.js , and Backend via Nodejs hosting it end to end.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers or deisgners involved."
+      ],
+    },
+    {
+      title: "React Native Developer",
+      company_name: "Team Robocon IIT Patna",
+      icon: robocon,
+      iconBg: "#E6DEDD",
+      date: " Sept 2023-onwards",
+      points: [
+        "Developing remote control application for Team Phoenix Abu Robocon robot",
+        "Collaborating with cross-functional teams including , Co-ordinators, Sub-cordinators.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    
+    
+  ];
+  const experiencesx = [
+    {
+      title: "ICSE",
+      company_name: "Matriculation",
+      icon: icse,
+      iconBg: "#383E56",
+      date: "April 2020",
+      points: [
+        "Scored 98.4%","Under Top 50 in State","Under Top 3 in District","Student of the Year, in School","The Times Awarded for Best Co-Curricular Activity"
+      ],
+    },
+    {
+      title: "CBSE",
+      company_name: "Higher Secondary",
+      icon: cbse ,
+      iconBg: "#E6DEDD",
+      date: "April 2020-June 2022",
+      points: [
+        "Scored 96%","Top 5 student in District"
+      ],
+    },
+    {
+      title: "Indian Institute of Technology(IIT), Patna ",
+      company_name: "Department of CSE, Undergrad with AI Major",
+      icon: iit ,
+      iconBg: "#383E56",
+      date: "August 2023- onwards",
+      points: [
+        "1st semester CPI 9.31 on a scale of 10",
+        "Started with research projects under professors",
+      ],
+    },
+    
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Course-selling-App",
+      description:
+        "Web-based platform that allows admin to create and push courses, and users to purchase it",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },{
+          name:"nodejs",
+          color: "green-text-gradient",
+        }
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Stock Prediction",
+      description:
+        "An End to End Model deployed on the web , to predict stock prices of next 30 days of a stock, rendered for next 30 ",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    
+  ];
+  
+  export { services, technologies, experiences,experiencesx, testimonials, projects };
